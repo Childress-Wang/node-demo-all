4f88201c38e40255b884d07c0d5285ee914bf878
pony1179 <1179664982@qq.com>
2021-11-26 18:32:48 +0800

    test

--- wPromise/test.js
+++ wPromise/test.js
@@ -3,8 +3,19 @@ const Promise = require('./index');
 let p1 = new Promise((resolve, reject)=>{
     setTimeout(()=>{
         resolve(200)
-    },5000)
+    },3000)
 }).then((res)=>{
-    console.log('res', res)
+    console.log('res', res);
+    return new Promise((resolve,reject)=> {
+        setTimeout(()=>{
+            reject(333)
+            // resolve(333)
+        }, 1000)
+    })
+},(err)=>{
+    console.log('err', err)
+}).then((res2)=>{
+    console.log('res2', res2);
+},(err)=>{
+    console.log('err2', err)
 })
-console.log(Promise)
